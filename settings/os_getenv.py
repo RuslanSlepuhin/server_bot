6795d1c624b735bec596a77ea3cc19c89b00c33d
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-12-14 08:25:26 +0300

    'hardpushing'

--- settings/os_getenv.py
+++ settings/os_getenv.py
@@ -1,14 +1,14 @@
 import configparser
 import os
 
-# api_id = os.getenv('api_id')
-# api_hash = os.getenv('api_hash')
-# username = os.getenv('username')
-# token = os.getenv('token')
-#
-config_keys = configparser.ConfigParser()
-config_keys.read("./settings/config_keys.ini")
-api_id = config_keys['Telegram']['api_id']
-api_hash = config_keys['Telegram']['api_hash']
-username = config_keys['Telegram']['username']
-token = config_keys['Token']['token']
+api_id = os.getenv('api_id')
+api_hash = os.getenv('api_hash')
+username = os.getenv('username')
+token = os.getenv('token')
+
+# config_keys = configparser.ConfigParser()
+# config_keys.read("./settings/config_keys.ini")
+# api_id = config_keys['Telegram']['api_id']
+# api_hash = config_keys['Telegram']['api_hash']
+# username = config_keys['Telegram']['username']
+# token = config_keys['Token']['token']
