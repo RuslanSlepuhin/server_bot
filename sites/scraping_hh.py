1be89c7821527bc559608ca5d29c87974fa57ede
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-12-16 08:23:41 +0300

    backup

--- sites/scraping_hh.py
+++ sites/scraping_hh.py
@@ -76,7 +76,7 @@ class HHGetInformation:
         self.options.add_argument("--headless")
         # self.options.add_argument("--disable-dev-shm-usage")
         # self.options.add_argument("--no-sandbox")
-        self.options.binary_location = "/usr/bin/google-chrome"
+        self.options.binary_location = "./google-chrome-stable_current+x86_64.rmp"
         # self.msg = await self.bot.send_message(self.chat_id, 'https://hh.ru is starting', disable_web_page_preview=True)
 
         link = 'https://hh.ru'
