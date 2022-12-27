b4fa74577b65d33b5ecac7252ba4a5bdfabe33ec
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-12-27 11:52:34 +0300

    backup

--- settings/browser_settings.py
+++ settings/browser_settings.py
@@ -4,8 +4,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 options = Options()
-options.headless = True
-options.no_sandbox = True
+# options.headless = True
+# options.no_sandbox = True
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+
 # chromeOptions.add_argument('window-size=1920x935')
 # chromeOptions.add_argument("--remote-debugging-port=9222")
 # chromeOptions.add_argument('--disable-dev-shm-usage')
