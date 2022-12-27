5a82b4d65489ee97a0207fe7d91e3ae1e6b4a4e4
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-12-27 11:26:54 +0300

    backup

--- settings/browser_settings.py
+++ settings/browser_settings.py
@@ -22,7 +22,7 @@ options.no_sandbox = True
 #     chrome_options=chromeOptions
 # )
 browser = webdriver.Firefox(
-    executable_path='/root/itcoty_bot/server_bot/settings/firefox_driver/geckodriver',
+    executable_path=r'/root/itcoty_bot/server_bot/settings/firefox_driver/geckodriver',
     options=options,
 )
 
