5c9ed0732cee3170cfc8190cf61f65f2b7c77257
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-12-27 12:33:03 +0300

    backup

--- sites/scraping_hh.py
+++ sites/scraping_hh.py
@@ -69,13 +69,14 @@ class HHGetInformation:
         :param db_tables:
         :return:
         """
+        self.browser.delete_all_cookies()
+        print('all cookies have deleted')
         self.db_tables = db_tables
 
         self.count_message_in_one_channel = 1
 
         link = 'https://hh.ru/search/vacancy?no_magic=true&L_save_area=true&text=&excluded_text=&salary=&currency_code=RUR&experience=doesNotMatter&schedule=remote&order_by=relevance&search_period=1&items_on_page=200&page=39&hhtmFrom=vacancy_search_list'
         response_dict = await self.get_info(link)
-        self.browser.delete_all_cookies()
 
     async def get_info(self, link):
 
