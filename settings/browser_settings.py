c9208a5c74ed679c01cbd63ad1673a636cec04f2
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-12-27 12:23:22 +0300

    backup

--- settings/browser_settings.py
+++ settings/browser_settings.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.firefox.options import Options
 
 options = Options()
 # options.headless = True
@@ -13,14 +12,13 @@ options.add_argument('--no-sandbox')
 # options.add_argument('window-size=1920x935')
 # chromeOptions.add_argument('window-size=1920x935')
 # chromeOptions.add_argument("--remote-debugging-port=9222")
-# chromeOptions.add_argument('--disable-dev-shm-usage')
 # browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-browser = webdriver.Firefox(
-    executable_path='/root/itcoty_bot/server_bot/settings/firefox_driver/geckodriver',
-    options=options
-)
-# browser = webdriver.Chrome(
-#     executable_path='/root/itcoty_bot/server_bot/utils/chromedriver/chromedriver',
+# browser = webdriver.Firefox(
+#     executable_path='/root/itcoty_bot/server_bot/settings/firefox_driver/geckodriver',
 #     options=options
 # )
+browser = webdriver.Chrome(
+    executable_path='/root/itcoty_bot/server_bot/utils/chromedriver/chromedriver',
+    options=options
+)
