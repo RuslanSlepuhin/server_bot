a2fa11b09e30c73c2a11eee17ce2938e753e4783
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-12-27 11:13:24 +0300

    backup

--- settings/browser_settings.py
+++ settings/browser_settings.py
@@ -1,25 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
+
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.service import Service as ChromeService
 
 
-chromeOptions = webdriver.ChromeOptions()
-chromeOptions.add_argument("--headless")
-chromeOptions.add_argument('--no-sandbox')
+options = webdriver.options()
+options.headless = True
+options.no_sandbox = True
 # chromeOptions.add_argument('window-size=1920x935')
 
 # chromeOptions.add_argument("--remote-debugging-port=9222")
 # chromeOptions.add_argument('--disable-dev-shm-usage')
 
-browser = webdriver.Chrome(
-    executable_path='/root/itcoty_bot/server_bot/utils/chromedriver/chromedriver',
-    chrome_options=chromeOptions
+# browser = webdriver.Chrome(
+#     executable_path='/root/itcoty_bot/server_bot/utils/chromedriver/chromedriver',
+#     chrome_options=chromeOptions
+# )
+browser = webdriver.Firefox(
+    executable_path='./settings/firefox_driver/geckodriver',
+    chrome_options=options
 )
-#
+
 # options = Options()
 # options.add_argument('headless')
 # options.add_argument('--no-sandbox')
