8bcdfe3ee915e559fce8e36172b2aff23df02ffc
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-12-27 11:50:51 +0300

    backup

--- settings/browser_settings.py
+++ settings/browser_settings.py
@@ -1,38 +1,30 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options
-
+from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.service import Service as ChromeService
-
 
 options = Options()
 options.headless = True
 options.no_sandbox = True
 # chromeOptions.add_argument('window-size=1920x935')
-
 # chromeOptions.add_argument("--remote-debugging-port=9222")
 # chromeOptions.add_argument('--disable-dev-shm-usage')
 
-# browser = webdriver.Chrome(
-#     executable_path='/root/itcoty_bot/server_bot/utils/chromedriver/chromedriver',
-#     chrome_options=chromeOptions
 # )
 options = Options()
 options.headless = True
 options.no_sandbox = True
 # options.add_argument('window-size=1920x935')
+# chromeOptions.add_argument('window-size=1920x935')
+# chromeOptions.add_argument("--remote-debugging-port=9222")
+# chromeOptions.add_argument('--disable-dev-shm-usage')
 # browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-browser = webdriver.Firefox(
-    executable_path='/root/itcoty_bot/server_bot/settings/firefox_driver/geckodriver',
+
+# browser = webdriver.Firefox(
+#     executable_path='/root/itcoty_bot/server_bot/settings/firefox_driver/geckodriver',
+#     options=options
+# )
+browser = webdriver.Chrome(
+    executable_path='/root/itcoty_bot/server_bot/utils/chromedriver/chromedriver',
     options=options
 )
-
-# options = Options()
-# options.add_argument('headless')
-# options.add_argument('--no-sandbox')
-# options.add_argument('window-size=1920x935')
-# browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
