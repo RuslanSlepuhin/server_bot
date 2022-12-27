214eede88cef91c2f550e86d3916887e77a452bf
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-12-27 12:27:57 +0300

    backup

--- settings/browser_settings.py
+++ settings/browser_settings.py
@@ -7,7 +7,7 @@ options = Options()
 # options.no_sandbox = True
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
-# options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-dev-shm-usage")
 # options.add_argument("--window-size=1920x1080")
 # options.add_argument('window-size=1920x935')
 # chromeOptions.add_argument('window-size=1920x935')
