e27cfd0b24046d8289647bae9d9c6dc3083baad8
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-12-30 13:43:22 +0300

    backup

--- endpoints/endpoints.py
+++ endpoints/endpoints.py
@@ -45,4 +45,4 @@ def get_from_db():
     return response
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='87.249.53.232', port=int(os.environ.get('PORT', 5000)))
