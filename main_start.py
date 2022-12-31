d232ce74a30979a0543a709ca5609cb0748f6952
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-12-31 16:31:52 +0300

    backup

--- main_start.py
+++ main_start.py
@@ -19,11 +19,21 @@ def start_bot():
     run()
     print('1-2')
 
+def start_talking_bot_run():
+    time.sleep(5)
+    print('3')
+    time.sleep(1)
+    print('3-1')
+    talking_bot_run()
+    print('3-2')
+
 
 def start_endpoints():
     # ev.wait()
     print('2')
     endpoints.endpoints.run_endpoints()
+
+
 
 # lock = Lock()
 #
@@ -31,7 +41,7 @@ if __name__ == "__main__":
 
     p1 = Process(target=start_endpoints, args=())
     p2 = Process(target=start_bot, args=())
-    p3 = Process(target=talking_bot_run, args=())
+    p3 = Process(target=start_talking_bot_run, args=())
 
     p1.start()
     p2.start()
