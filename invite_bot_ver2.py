7b6190206333a7e6536b132f4934ae11ba5ab8d0
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-01-22 14:23:14 +0300

    backup

--- invite_bot_ver2.py
+++ invite_bot_ver2.py
@@ -57,7 +57,7 @@ api_hash_double = settings.api_hash_double
 username_double = settings.username_double
 
 client = TelegramClient(username_double, int(api_id_double), api_hash_double)
-
+client.connect()
 # token = settings.token
 # logging.basicConfig(level=logging.INFO)
 # bot_aiogram = Bot(token=token)
