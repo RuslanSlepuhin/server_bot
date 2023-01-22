04c34bba648d95cbcd8fdc8f674ab92c3cef1478
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-01-22 15:29:47 +0300

    backup

--- main_start.py
+++ main_start.py
@@ -24,14 +24,16 @@ def start_endpoints():
 
 if __name__ == "__main__":
 
-    p1 = Process(target=start_endpoints, args=())
-    p2 = Process(target=start_bot, args=())
-    p3 = Process(target=start_bot, args=(True, settings.token_red))
-    p4 = Process(target=talking_bot_run, args=())
+    start_bot(double=True, token_in=settings.token_red)
+
+    # p1 = Process(target=start_endpoints, args=())
+    # p2 = Process(target=start_bot, args=())
+    # p3 = Process(target=start_bot, args=(True, settings.token_red))
+    # p4 = Process(target=talking_bot_run, args=())
 
     # p1.start()
     # p2.start()
-    p3.start()
+    # p3.start()
     # p4.start()
 
     # p1.join()
