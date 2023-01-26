54596c5b6e7c736cd3aaa2321785d94e958aa3d5
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-01-26 16:19:37 +0300

    flood_control logs

--- utils/tg_channels/links.py
+++ utils/tg_channels/links.py
@@ -1,4 +1,12 @@
 list_links = [
+    'https://t.me/remoters',
+    'https://t.me/young_relocate',
+    'https://t.me/workvc',
+    'https://t.me/forproducts',
+    'https://t.me/dreamjob_minsk',
+    'https://t.me/juno_jobs',
+    'https://t.me/jobforjunior',
+    'https://t.me/noexperience',
     'https://t.me/marketing_jobs',
     'https://t.me/myresume_ru',
     'https://t.me/jvmjobs',
