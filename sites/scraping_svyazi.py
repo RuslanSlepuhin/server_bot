416eb94950b9779da6e8753f44b2d2b222eda823
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-01-26 20:59:38 +0300

    flood_control logs

--- sites/scraping_svyazi.py
+++ sites/scraping_svyazi.py
@@ -352,9 +352,10 @@ class SvyaziGetInformation:
         await self.output_logs(
             response_from_db=response_from_db,
             vacancy=vacancy,
+            vacancy_url=vacancy_url
         )
 
-    async def output_logs(self, response_from_db, vacancy, word=None):
+    async def output_logs(self, response_from_db, vacancy, word=None, vacancy_url=None):
 
         additional_message = ''
         profession = response_from_db['profession']
@@ -366,7 +367,7 @@ class SvyaziGetInformation:
 
         elif not response_from_db:
             prof_str = ", ".join(profession['profession'])
-            additional_message = f"<b>+w: {prof_str}</b>\n{profession['tag']}\n{profession['anti_tag']}\n"
+            additional_message = f"<b>+w: {prof_str}</b>\n{vacancy_url}\n{profession['tag']}\n{profession['anti_tag']}\n"
 
             if 'no_sort' not in profession['profession']:
                 self.written_vacancies += 1
@@ -382,13 +383,6 @@ class SvyaziGetInformation:
                 msg=self.current_message
             )
 
-            # self.current_message = await self.bot.edit_message_text(
-            #     f'{self.current_message.text}{new_text}',
-            #     self.current_message.chat.id,
-            #     self.current_message.message_id,
-            #     parse_mode='html',
-            #     disable_web_page_preview=True
-            # )
         else:
             new_text = f"{self.count_message_in_one_channel}. {vacancy}\n{additional_message}"
             self.current_message = await send_message(
@@ -396,9 +390,6 @@ class SvyaziGetInformation:
                 chat_id=self.chat_id,
                 text=new_text
             )
-
-            # self.current_message = await self.bot.send_message(self.chat_id,
-            #                                                    f"{self.count_message_in_one_channel}. {vacancy}\n{additional_message}")
 
         print(f"\n{self.count_message_in_one_channel} from_channel hh.ru search {word}")
         self.count_message_in_one_channel += 1
