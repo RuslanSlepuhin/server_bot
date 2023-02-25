8be675044ec9994a22a654536ca052e501409df5
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-02-25 12:43:03 +0300

    210223 fix merge previous short

--- utils/additional_variables/additional_variables.py
+++ utils/additional_variables/additional_variables.py
@@ -171,6 +171,7 @@ help_text = '/log or /logs - get custom logs (useful for developer\n' \
             '🔆/geek - get data from geek.ru\n' \
             '🔆/remotehub - get data from www.remotehub.com\n' \
             '🔆/remotejob - get data from remote-job.ru\n' \
+            '🔆/ingame - get data from ingame' \
             '---------------------------------------------------\n\n' \
             '/download - ❗️you get excel from admin vacancies with search tags\n' \
             '/ambulance - if bot gets accident in hard pushing and you think you loose the shorts\n\n' \
