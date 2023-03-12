2bc435480dad3106e62154ea3ded3c741cd402aa
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-03-12 22:17:31 +0300

    120323 bug shorts fixing

--- invite_bot_ver2.py
+++ invite_bot_ver2.py
@@ -5052,8 +5052,7 @@ class InviteBot():
                         await self.write_to_logs_error(f"Results:\n{short}\n")
                         try:
                             await self.bot_aiogram.send_message(
-                                # config['My_channels'][f'{profession_channel}_channel'],
-                                variable.channel_id_for_shorts,
+                                config['My_channels'][f'{profession_channel}_channel'],
                                 short,
                                 parse_mode='html',
                                 disable_web_page_preview=True
