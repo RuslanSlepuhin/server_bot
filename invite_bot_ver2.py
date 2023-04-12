54ace6fb66294103dd037287bcc4d7c8cce8958b
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-04-12 11:02:04 +0300

    user id

--- invite_bot_ver2.py
+++ invite_bot_ver2.py
@@ -4469,8 +4469,6 @@ class InviteBot():
     async def send_file_to_user(self, message, path, caption='Please take it', send_to_developer=False):
         logs.write_log(f"invite_bot_2: function: send_file_to_user")
         with open(path, 'rb') as file:
-            await self.client.send_file(int(variable.developer_chat_id), file, caption=caption)
-
             try:
                 await self.bot_aiogram.send_document(message.chat.id, file, caption=caption)
                 if send_to_developer and message.chat.id != variable.developer_chat_id:
