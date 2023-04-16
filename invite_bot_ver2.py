6f3882db2efd549aeea41cb49226ac7e806cd3b7
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-04-16 22:41:17 +0300

    run parser when start

--- invite_bot_ver2.py
+++ invite_bot_ver2.py
@@ -285,7 +285,7 @@ class InviteBot():
             await self.bot_aiogram.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!', reply_markup=parsing_kb)
             await self.bot_aiogram.send_message(variable.id_owner, f'User {message.from_user.id} has started')
             config2 = configparser.ConfigParser()
-            config2.read("./settings/config.keys.ini")
+            config2.read("./settings/config_keys.ini")
             if self.token == config2['Token']['token_red']:
                 await get_news(message=message)
                 pass
