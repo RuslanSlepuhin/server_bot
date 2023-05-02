c62d8cf3d43324ed6d06b53f06bcac64662bc64c
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-05-02 10:46:40 +0300

    upgrade020523

--- invite_bot_ver2.py
+++ invite_bot_ver2.py
@@ -3985,11 +3985,11 @@ class InviteBot():
         with open(path, 'rb') as file:
             try:
                 await self.bot_aiogram.send_document(message.chat.id, file, caption=caption)
-                if send_to_developer and message.chat.id != variable.developer_chat_id:
-                    try:
-                        await self.bot_aiogram.send_document(int(variable.developer_chat_id), file, caption=caption)
-                    except Exception as e:
-                        print(e)
+                # if send_to_developer and message.chat.id != variable.developer_chat_id:
+                #     try:
+                #         await self.bot_aiogram.send_document(int(variable.developer_chat_id), file, caption=caption)
+                #     except Exception as e:
+                #         print(e)
             except:
                 await self.client.send_file(int(variable.developer_chat_id), file, caption=caption)
 
