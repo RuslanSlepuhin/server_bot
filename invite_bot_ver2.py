4576b62e70078c30a7e1fcc3d7ea7aed28ff588a
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-05-17 15:01:26 +0300

    160523

--- invite_bot_ver2.py
+++ invite_bot_ver2.py
@@ -1686,7 +1686,7 @@ class InviteBot():
             if callback.data == 'go_by_admin': # next step if callback.data[2:] in self.valid_profession_list:
                 # make the keyboard with all professions
                 # if int(callback.message.from_user.id) in variable.white_admin_list:
-                #     self.markup = await compose_inline_keyboard(prefix='admin')
+                self.markup = await compose_inline_keyboard(prefix='admin')
                 await self.bot_aiogram.send_message(callback.message.chat.id, 'choose the channel for vacancy checking', reply_markup=self.markup)
                 # else:
                 #     await self.bot_aiogram.send_message(callback.message.chat.id, "Sorry, You have not the permissions")
