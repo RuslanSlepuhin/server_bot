f823afb2b1fca414b8dca708937f09bf9a230dd4
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-06-16 00:38:07 +0300

    13062023

--- sites/scraping_rabota.py
+++ sites/scraping_rabota.py
@@ -173,9 +173,9 @@ class RabotaGetInformation:
                                 body = soup.find('div', class_='vacancy-description').get_text()
                             except:
                                 return False
-
-                        body = body.replace('\n\n', '\n')
-                        body = re.sub(r'\<[A-Za-z\/=\"\-\>\s\._\<]{1,}\>', " ", body)
+                        if body:
+                            body = body.replace('\n\n', '\n')
+                            body = re.sub(r'\<[A-Za-z\/=\"\-\>\s\._\<]{1,}\>', " ", body)
 
                         tags = ''
                         try:
