218f13e3c07c81ed0bd8d44706e4c91104eb3056
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-06-27 15:52:42 +0300

    210623

---
+++ utils/custom_subs/custom_subs.py
@@ -0,0 +1,34 @@
+custom_subs = {
+    'analyst': ['sys_analyst', 'data_analyst', 'data_scientist', 'ba'],
+    'backend': ['python', 'c', 'php', 'java', 'ruby', 'scala', 'net', 'nodejs', 'laravel', 'golang', 'delphi', 'abap', 'ml', 'data_engineer', 'unity', 'one_c', 'embedded'],
+    'designer': ['ui_ux', 'motion', 'dd', 'ddd', 'game_designer', 'illustrator', 'graphic', 'uxre_searcher'],
+    'devops': [],
+    'frontend': ['vue', 'react', 'angular', 'wordpress', 'bitrix', 'joomla', 'drupal'],
+    'game': [],
+    'hr': [],
+    'marketing': ['smm', 'copyrighter', 'seo', 'link_builder', 'media_buyer', 'email_marketer', 'context', 'content_manager', 'tech_writer'],
+    'mobile': ['ios', 'android', 'cross_mobile', 'flutter', 'react_native'],
+    'pm': ['project', 'product'],
+    'product': ['project', 'product'],
+    'qa': ['manual_qa', 'aqa', 'support'],
+    'sales_manager': [],
+    'junior': []
+}
+
+name_professions = {
+    'analyst': 'Аналитики',
+    'backend': 'Backend разработчики',
+    'designer': 'Дизайнеры',
+    'devops': 'DevOps',
+    'frontend': 'Frontend разработчики',
+    'game': 'Game разработчики',
+    'hr': 'HR',
+    'marketing': 'Маркетологи',
+    'mobile': 'Mobile разработчики',
+    'pm': 'Product & project менеджеры',
+    'product': 'Product & project менеджеры',
+    'qa': 'QA',
+    'sales_manager': 'Sales',
+    'junior': 'Junior'
+
+}
