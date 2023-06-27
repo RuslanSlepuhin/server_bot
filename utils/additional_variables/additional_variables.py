d31faeca9a2a48bb3ac32682b76c8b0cc651cff8
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-06-27 09:07:21 +0300

    210623

--- utils/additional_variables/additional_variables.py
+++ utils/additional_variables/additional_variables.py
@@ -34,8 +34,8 @@ tables_for_search_vacancy_existing = [admin_database, 'archive']
 # all_tables_for_vacancy_search = ['designer', 'game', 'product', 'mobile', 'pm', 'sales_manager', 'analyst', 'frontend',
 #                      'marketing', 'devops', 'hr', 'backend', 'qa', 'junior', admin_database, archive_database]
 
-profession_list_for_pushing_by_schedule = ['marketing', 'sales_manager', 'analyst', 'designer', 'pm', 'qa', 'devops',
-                                           'mobile', 'game', 'frontend', 'backend']
+profession_list_for_pushing_by_schedule = ['hr', 'game', 'marketing', 'sales_manager', 'analyst', 'designer', 'pm', 'qa', 'devops',
+                                           'mobile', 'frontend', 'backend']
 all_tables_for_vacancy_search = []
 all_tables_for_vacancy_search.extend([admin_database, archive_database])
 all_tables_for_vacancy_search.extend(valid_professions)
