05ac32bc0b167fb3231c3309af60eaa7cbba3d51
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-08-21 15:38:28 +0300

    140723

--- _apps/endpoints/endpoints.py
+++ _apps/endpoints/endpoints.py
@@ -122,7 +122,7 @@ class Endpoints:
         @app.route("/vacancy", methods = ['GET'])
         async def get_single_vacancy_for_web_vacancy():
             print("124 vacancy")
-            await asyncio.sleep(1)
+            await asyncio.sleep(0.2)
             vacancy_id = request.args.get('id')
             return await get_single_vacancies_for_web(vacancy_id)
 
