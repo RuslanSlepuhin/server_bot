201fbf4a0f742845b8d3190a69e1898b34364ce4
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-08-22 09:51:56 +0300

    140723

--- main_start.py
+++ main_start.py
@@ -1,5 +1,6 @@
+import configparser
 import os
-import time
+from _apps.amin_panel_tg_view.views.bot.bot_view import BotView
 from _apps.talking_bot.mvp_connect_talking_bot import talking_bot_run
 from invite_bot_ver2 import run as run_parser_bot
 from _apps.endpoints import endpoints
@@ -11,7 +12,7 @@ num_processes = os.cpu_count()
 
 def start_bot(double=False, token_in=None):
     # time.sleep(3)
-    print('1')
+    print('main_bot is starting')
     run_parser_bot(
         double=double,
         token_in=token_in
@@ -19,8 +20,17 @@ def start_bot(double=False, token_in=None):
     print('bot has been stopped')
 
 def start_endpoints():
-    print('2')
+    print('endpoints are starting')
     endpoints.run_endpoints()
+
+def start_admin_panel():
+    print('admin panel is starting')
+    config = configparser.ConfigParser()
+    config.read("_apps/amin_panel_tg_view/settings/config.ini")
+    __token = config['Bot']['token']
+
+    bot = BotView(token=__token)
+    bot.handlers()
 
 if __name__ == "__main__":
 
@@ -30,11 +40,13 @@ if __name__ == "__main__":
     p2 = Process(target=start_bot, args=())
     p3 = Process(target=start_bot, args=(True, settings.token_red))
     p4 = Process(target=talking_bot_run, args=())
+    p5 = Process(target=start_admin_panel, args=())
 
     p1.start()
     p4.start()
     p2.start()
     p3.start()
+    p5.start()
 
     # start_bot(double=True, token_in=settings.token_red)
 
