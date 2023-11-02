64b25475b65f6bd207964ba3fd5c66f2b11f55e8
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-11-02 09:25:32 +0300

    140723

---
+++ _apps/coffee_customer_bot_apps/endpoints/requests_methods.py
@@ -0,0 +1,66 @@
+import requests
+from _apps.coffee_customer_bot_apps.variables import variables
+
+
+class RequestsClass:
+
+    def __init__(self):
+        pass
+
+    # -------------- Customer side --------------
+
+    def customer_pressed_cancel_button(self):
+        requests.get(variables.customer_pressed_cancel_endpoint)
+        return {}
+
+    def customer_feedback(self, rating=None, feedback=None):
+        if rating or feedback:
+            requests.get(variables.customer_feedback_endpoint)
+        return {}
+
+
+    def customer_add_username_to_database(self, user_id):
+        requests.get(variables.customer_add_username_to_database_endpoint)
+        return {}
+
+
+    # -------------- Horeca side --------------
+
+    # Barista rejects the order
+    def horeca_status_reject_order(self):
+        requests.get(variables.main_endpoint)
+        return {}
+
+    # Barista confirms the order
+    def horeca_status_confirm_order(self):
+        # взял в работу
+        requests.get(variables.main_endpoint)
+        return {}
+
+    # Barista: the order processes
+    def horeca_status_order_processes(self):
+        requests.get(variables.main_endpoint)
+        return {}
+
+    # Barista: the order ready
+    def horeca_status_order_is_ready(self):
+        requests.get(variables.main_endpoint)
+        return {}
+
+    # Barista: the order ready
+    def horeca_status_order_has_been_taken(self):
+        requests.get(variables.main_endpoint)
+        return {}
+
+    def horeca_status_goods_broken(self):
+        requests.get(variables.main_endpoint)
+        return {}
+
+    def horeca_disassembles_the_goods(self):
+        requests.get(variables.main_endpoint)
+        return {}
+
+    def horeca_confirms_customer_cancel(self):
+        requests.get(variables.main_endpoint)
+        return {}
+
---
+++ _apps/coffee_customer_bot_apps/variables/variables.py
@@ -0,0 +1,20 @@
+
+# Requests variables
+customer_pressed_cancel_endpoint = "https://test.ru"
+customer_feedback_endpoint = "https://test.ru"
+customer_add_username_to_database_endpoint = "https://test.ru"
+
+# Endpoints variables
+is_user_active_endpoint = "/is_user_active"
+provide_message_to_user_endpoint = "/provide_message_to_user"
+impossible_to_cancel_order_endpoints = "/impossible_to_cancel_order"
+
+provide_message_to_horeca_endpoint = "/provide_message_to_horeca"
+
+
+main_endpoint = "http://127.0.0.1:5000/is_user_active"
+
+#--------------- server urls ----------------------
+server_test_status_endpoint_from_customer = "/status_from_user/"
+server_test_status_endpoint_from_horeca = "/status_from_horeca/"
+server_domain = "https://2558-178-124-175-66.ngrok-free.app/client"
