710ab30128184214d461d57ab049b050ac38b45a
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-01-29 14:14:09 +0300

    webformbot

--- _apps/webForm/form/views.py
+++ _apps/webForm/form/views.py
@@ -16,7 +16,7 @@ def home(request):
   try:
     print(bot_domain, external_web_hook, server_domain, endpoint_form, form_page)
   except Exception as ex:
-    print ex
+    print(ex)
   return render(request, 'home.html', {'server_domain': server_domain, 'endpoint_form': endpoint_form})
 
 def form_is_completed(request):
