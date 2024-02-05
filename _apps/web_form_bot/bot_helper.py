6fa6b47f0ca8146d878b486464cfd692334e6abd
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-02-05 22:39:27 +0300

    webformbot

--- _apps/web_form_bot/bot_helper.py
+++ _apps/web_form_bot/bot_helper.py
@@ -58,7 +58,12 @@ class HelperBot:
     async def text_object_from_form(self, data:dict) -> str:
         text = ""
         for key in data:
-            text += f"{variables.keys_as_questions[key]}\n- {data[key]}\n\n" if key in variables.keys_as_questions else f"{key}: {data[key]}\n\n"
+            if key.lower() == 'submit':
+                text += f"Form: {data[key]}\n"
+            elif key.lower() == "form":
+                text += f"{key}: {data[key]}\n"
+            else:
+                text += f"{variables.keys_as_questions[key]}\n- {data[key]}\n\n" if key in variables.keys_as_questions else f"{key}: {data[key]}\n\n"
         return text
 
 
