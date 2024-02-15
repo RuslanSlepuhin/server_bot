04f8d6e8bef199963ad933f28ce75616aabb7348
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-02-15 22:04:10 +0300

    horeca_bot

--- main_start.py
+++ main_start.py
@@ -69,7 +69,7 @@ def form_app_start():
 
 if __name__ == "__main__":
 
-    # p1 = Process(target=start_endpoints, args=())
+    p1 = Process(target=start_endpoints, args=())
     p2 = Process(target=start_bot, args=())
     p3 = Process(target=start_bot, args=(True, settings.token_red))
     p4 = Process(target=start_admin_panel, args=())
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     # p11 = Process(target=form_app_start, args=())
 
 
-    # p1.start()
+    p1.start()
     p2.start()
     p3.start()
     p4.start()
