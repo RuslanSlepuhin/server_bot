0e0911cad2405622446bdeda2540d3f2e6560582
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-03-09 14:46:26 +0300

    horeca_bot

--- _apps/webForm/form/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-urlpatterns = [
-    path('form/', webFormView.as_view()),
-    path('home/', home, name='home'),
-    path('form_is_completed/', form_is_completed, name='form_is_completed'),
-    ]
--- sites/__brainporteind.py
+++
@@ -1,1 +0,0 @@
-link = 'https://brainporteindhoven.com/int/for-you/work/jobs/jobs-in-it?gclid=CjwKCAjw79iaBhAJEiwAPYwoCK3FhRi17RDS_PE04NGWc6N4i9-AE8s6em4URhfu6UIZR9CNsMNauRoCt2EQAvD_BwE'
