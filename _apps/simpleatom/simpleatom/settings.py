3e76bf082fb60c694b6ed4f7dfae118329c49f8f
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-03-10 10:32:15 +0300

    cors

--- _apps/simpleatom/simpleatom/settings.py
+++ _apps/simpleatom/simpleatom/settings.py
@@ -38,7 +38,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1",
     "http://localhost",
-    "http://simpleatom.io"
+    "http://simpleatom.io",
+    "https://simpleatom.io",
 ]
 
 
