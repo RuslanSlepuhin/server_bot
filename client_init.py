6bb59a6cac8a4f279381166af0ed300911138e1d
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-03-18 14:35:17 +0300

    tel_init

--- client_init.py
+++ client_init.py
@@ -1,9 +1,28 @@
 import asyncio
+import configparser
+
+from telethon import TelegramClient
+
 from telegram_chats.scraping_telegramchats2 import WriteToDbMessages
 
 def client_init():
     client_class = WriteToDbMessages()
     asyncio.run(client_class.client_init())
+
+def admin_panel_init():
+    config = configparser.ConfigParser()
+    config.read("./_apps/admin_panel_tg_view/settings/config.ini")
+
+    api_id = config['Telegram_api']['api_id']
+    api_hash = config['Telegram_api']['api_hash']
+    username = config['Telegram_api']['username']
+
+    client = TelegramClient(username, int(api_id), api_hash)
+    asyncio.run(client.start())
+    print("connection done")
+    client.disconnect()
+    print("disconnect")
+
 
 if __name__ == "__main__":
     client_init()
