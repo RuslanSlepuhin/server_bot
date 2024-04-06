ed9016aa93b60ed5baefade114fbdcf8261afe38
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-04-06 19:15:28 +0300

    simpleatom

--- _apps/simpleatom/form/variables/variables.py
+++ _apps/simpleatom/form/variables/variables.py
@@ -1,6 +1,6 @@
-# from _debug import debug
+from _debug import debug
 
 actual_ngrok_tunnel = "https://1000-178-122-196-59.ngrok-free.app"
 common_field_name = 'data'
-bot_domain = "https://4dev.itcoty.ru"# if not debug else actual_ngrok_tunnel
+bot_domain = "https://4dev.itcoty.ru" if not debug else actual_ngrok_tunnel
 external_api = "/external"
