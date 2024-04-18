c2f0bbb38c7f75310c4fc5181b871e55f094737a
Mixail132 <millionbaxov@gmail.com>
2024-04-18 16:22:28 +0300

    Change the query string parameters
    These parameters deffer the query filter, when looking for vacancies on hh.ru.

--- sites/scraping_hh.py
+++ sites/scraping_hh.py
@@ -74,12 +74,13 @@ class HHGetInformation:
         self.debug = False
         self.additional = (f"/search/vacancy?"
                            f"search_field=name&"          # Искать совпадениев названии вакансии
-                           f"enable_snippets=true&"       # с ревью вакансий в поисковой выдаче
+                           f"enable_snippets=false&"      # без ревью вакансий в поисковой выдаче
                            f"area=0&"                     # по всем регионам
                            f"ored_clusters=true&"         # 
-                           f"search_period=3&"            # за последние 3 дня
-                           f"order_by=publication_time&"  # сортировать по убыванию даты публикации ?
+                           f"search_period=1&"            # за последние 3 дня
+                           f"items_on_page=100&"          # количество вакансий на странице
                            f"text=**word&"                # по ключевому слову
+                           f"order_by=publication_time&"  # сортировать по убыванию даты публикации ?
                            f"page=**page"                 # номер страницы
                            )
         self.main_class = kwargs['main_class']
