051c3dd30a1793e5d5d9e0facc37ac43376d795d
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-04-18 17:19:05 +0300

    hh

--- sites/scraping_hh.py
+++ sites/scraping_hh.py
@@ -209,7 +209,6 @@ class HHGetInformation:
     async def get_content_from_link(self, return_raw_dictionary=False):
         self.found_by_link = 0
         for link in self.list_links:
-            await self.report.collect_parser_links(link)
             try:
                 vacancy_url = link.get('href')
             except:
