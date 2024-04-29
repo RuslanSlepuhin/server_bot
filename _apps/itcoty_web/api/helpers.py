789b2a66b9f125b84b6be1fbf6f110c2b23e8886
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-04-29 11:59:06 +0300

    -auth

--- _apps/itcoty_web/api/helpers.py
+++
@@ -1,9 +0,0 @@
-from collections.abc import MutableMapping
-from typing import List, Dict
-
-def add_numeration_to_response(queryset: List[MutableMapping]) -> Dict:
-    result = {}
-    for n in range(len(queryset)):
-        result[str(n)] = queryset[n]
-
-    return result
