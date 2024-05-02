f2d890bfa61858807a0f5fdd728c2fa459b363ec
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-05-02 21:05:07 +0300

    auth

--- _apps/itcoty_web/itcoty_web/urls.py
+++ _apps/itcoty_web/itcoty_web/urls.py
@@ -17,4 +17,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/v1/", include("api.urls"))]
+urlpatterns = [path("api/admin/", admin.site.urls), path("api/v1/", include("api.urls"))]
