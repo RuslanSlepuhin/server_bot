bc36da477e647fb1a211d3304f45dd1e68bb714b
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-05-23 19:34:41 +0300

    cors localhost:3000

--- _apps/itcoty_web/itcoty_web/settings.py
+++ _apps/itcoty_web/itcoty_web/settings.py
@@ -30,7 +30,12 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost:3000',
+]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 
 AUTH_USER_MODEL = "api.User"
 
