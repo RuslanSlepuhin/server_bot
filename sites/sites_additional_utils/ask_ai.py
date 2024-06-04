fc02612944e792f827c2a5d6b71018b6e75f8543
Mixail132 <millionbaxov@gmail.com>
2024-06-04 17:10:35 +0300

    Add new neural network to the project
    Now this is 'llama-3' for filtering the vacancies, cause 'gemini' banned

---
+++ sites/sites_additional_utils/ask_ai.py
@@ -0,0 +1,42 @@
+import json.decoder
+import json
+
+import requests
+from sites.sites_additional_utils.question import compose_question
+
+
+def ask_ai(question, text):
+    question_ai = compose_question(question, text)
+    url = "https://creativeai-68gw.onrender.com/chat"
+    data = {'query': f'{question_ai}', 'model': 'llama-3-70b'}
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(url, headers=headers, json=data)
+    byte_response = response.content
+    events = byte_response.split(b'\r\n\r\n')
+    answers = []
+    for event in events:
+        if event:
+            answer = event.decode()
+            answer = answer.replace("data: ", "")
+            answers.append(json.loads(answer))
+    for answer in answers:
+        if answer['event'] == "final-response":
+            return answer["data"]['message']
+
+
+if __name__ == "__main__":
+    trial_question = "Is IT?"
+    trial_text = """
+    Обучение.
+    Мы предлагаем:
+    Бесплатное обучение.
+    Карьерные перспективы: получи оффер на позицию младшего backend-разработчика в команду
+    Газпромбанка после успешного обучения;
+    Обучение у экспертов: ты сможешь развиваться в backend-разработке под
+    руководством опытных специалистов;
+    Профессиональное развитие: ты освоишь актуальные мировые стандарты по востребованным
+    стекам: Java 21, Kotlin, Spring Boot, PostgreSQL, Docker и k8s.
+    Обратная связь: ты будешь получать обратную связь по всем выполненным задачам.
+    """
+
+    ask_ai(trial_question, trial_text)
