3d44c9cb23d56d66d2c2838b9120aee0af1cfc79
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-06-04 12:35:05 +0300

    hh

--- settings/browser_settings.py
+++ settings/browser_settings.py
@@ -8,7 +8,7 @@ options = Options()
 # options.no_sandbox = True
 if not debug:
     options.add_argument('--headless')
-options.add_argument('--no-sandbox')
+# options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
 options.add_argument('--verbose')
