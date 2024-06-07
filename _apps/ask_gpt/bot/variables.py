ad376aab562c1e00221772aac703a857a914fdb4
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-06-07 22:41:56 +0300

    gpt

--- _apps/ask_gpt/bot/variables.py
+++ _apps/ask_gpt/bot/variables.py
@@ -3,3 +3,4 @@ chat_gpt_without_history = "without_history"
 chat_gpt_set_prompt = 'set_prompt'
 watch_prompt = 'watch_prompt'
 tokens_number = 'tokens_number'
+watch_tokens_number = 'watch_tokens_number'
