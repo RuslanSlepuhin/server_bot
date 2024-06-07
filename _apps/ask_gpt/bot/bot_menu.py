10d413aca4682077113297c251e7c5321bc6eb92
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-06-07 22:46:41 +0300

    gpt

--- _apps/ask_gpt/bot/bot_menu.py
+++ _apps/ask_gpt/bot/bot_menu.py
@@ -1,15 +1,18 @@
 from aiogram import types
 
-from _apps.ask_gpt.bot.variables import chat_gpt_sales_manager, chat_gpt_without_history, chat_gpt_set_prompt, watch_prompt
+from _apps.ask_gpt.bot.variables import chat_gpt_sales_manager, chat_gpt_without_history, chat_gpt_set_prompt, \
+    watch_prompt, tokens_number, watch_tokens_number
 
 
 async def set_default_commands(bot):
     await bot.set_my_commands(
         [
             types.BotCommand(command="start", description="Запустить бота"),
-            types.BotCommand(command=chat_gpt_sales_manager, description="Сэйл в Simpleatom (история суммарно 10 последних сообщений + PROMPT)"),
+            # types.BotCommand(command=chat_gpt_sales_manager, description="Сэйл в Simpleatom (история суммарно 10 последних сообщений + PROMPT)"),
             types.BotCommand(command=chat_gpt_without_history, description="Ответ на вопрос без сохранении истории"),
             types.BotCommand(command=chat_gpt_set_prompt, description="Установить PROMPT"),
             types.BotCommand(command=watch_prompt, description="Посмотреть установленный PROMPT"),
+            types.BotCommand(command=tokens_number, description="Установить TOKENS"),
+            types.BotCommand(command=watch_tokens_number, description="Посмотреть количество TOKENS"),
         ]
     )
