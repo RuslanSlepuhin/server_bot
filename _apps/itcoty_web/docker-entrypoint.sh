b14d7cfc3f2c2ee3132ca0e41b8e44e858172c7c
Mixail132 <millionbaxov@gmail.com>
2024-10-02 14:54:31 +0300

    Add collectstatic command, run Django server when starting.

--- _apps/itcoty_web/docker-entrypoint.sh
+++ _apps/itcoty_web/docker-entrypoint.sh
@@ -1,8 +1,12 @@
+
 echo "Apply migrations for api."
 python manage.py migrate api
 
 echo "Apply migrations for other apps."
 python manage.py migrate
 
-echo "Start gunicorn"
-gunicorn --bind 0.0.0.0:8000 itcoty_web.wsgi
+echo "Collect static files."
+python manage.py collectstatic --noinput
+
+echo "Start server."
+python manage.py runserver 0.0.0.0:8000
