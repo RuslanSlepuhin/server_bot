534b9e452d4f91ab1dec0db5cc679e6c9eff65b7
Mixail132 <millionbaxov@gmail.com>
2024-10-02 14:51:07 +0300

    Rename static folder.

--- _apps/itcoty_web/itcoty_web/dirs.py
+++ _apps/itcoty_web/itcoty_web/dirs.py
@@ -1,12 +1,11 @@
 from pathlib import Path
 
-MAIN_DIR = Path(__file__).resolve().parents[3]
 BASE_DIR = Path(__file__).resolve().parents[1]
 DOCKER_CACHE_DIR = Path("/var/cache/app")
 LOCAL_DIR = BASE_DIR / ".local"
 
 STATIC_DIR = (
                  DOCKER_CACHE_DIR if DOCKER_CACHE_DIR.is_dir() else LOCAL_DIR
-             ) / "django-static"
+             ) / "static"
 
 STATIC_DIR.mkdir(exist_ok=True, mode=0o777)
