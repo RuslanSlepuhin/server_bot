0f3606e684a102b3701190430e7b25e812575a82
Mixail132 <millionbaxov@gmail.com>
2024-10-02 21:14:21 +0300

    Add an enerypoint to create a Django superuser.

--- _apps/itcoty_web/docker-entrypoint.sh
+++ _apps/itcoty_web/docker-entrypoint.sh
@@ -5,6 +5,9 @@ python manage.py migrate api
 echo "Apply migrations for other apps."
 python manage.py migrate
 
+echo "Create a superuser."
+python manage.py createsuperuser-ifnot
+
 echo "Collect static files."
 python manage.py collectstatic --noinput
 
