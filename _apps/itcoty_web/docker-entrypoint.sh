bd971dda923201495f6002c23aacf2a3dfe57154
Mixail132 <millionbaxov@gmail.com>
2024-10-29 22:23:35 +0300

    Order the migrations applying.

--- _apps/itcoty_web/docker-entrypoint.sh
+++ _apps/itcoty_web/docker-entrypoint.sh
@@ -1,9 +1,33 @@
 
-echo "Apply migrations for api."
-python manage.py migrate api
+echo "Apply migrations for other apps."
+python manage.py migrate contenttypes
 
 echo "Apply migrations for other apps."
-python manage.py migrate
+python manage.py migrate auth
+
+echo "Apply migrations for other apps."
+python manage.py migrate api 0001_initial
+
+echo "Apply migrations for other apps."
+python manage.py migrate account
+
+echo "Apply migrations for other apps."
+python manage.py migrate admin
+
+echo "Apply migrations for other apps."
+python manage.py migrate authtoken
+
+echo "Apply migrations for other apps."
+python manage.py migrate sessions
+
+echo "Apply migrations for other apps."
+python manage.py migrate sites
+
+echo "Apply migrations for other apps."
+python manage.py migrate socialaccount
+
+echo "Apply migrations for api."
+python manage.py migrate api 0002_vacancies_certificate_filter_follower_quizz_and_more
 
 echo "Create a superuser."
 python manage.py createsuperuser-ifnot
