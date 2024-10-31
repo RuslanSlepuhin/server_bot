3ca946b517826e824bd4962d9886366d12e92428
Mixail132 <millionbaxov@gmail.com>
2024-10-31 15:58:46 +0300

    Reduce the migrations applying commands.

--- _apps/itcoty_web/docker-entrypoint.sh
+++ _apps/itcoty_web/docker-entrypoint.sh
@@ -1,33 +1,6 @@
 
-echo "Apply migrations for other apps."
-python manage.py migrate contenttypes
-
-echo "Apply migrations for other apps."
-python manage.py migrate auth
-
-echo "Apply migrations for other apps."
-python manage.py migrate api 0001_initial
-
-echo "Apply migrations for other apps."
-python manage.py migrate account
-
-echo "Apply migrations for other apps."
-python manage.py migrate admin
-
-echo "Apply migrations for other apps."
-python manage.py migrate authtoken
-
-echo "Apply migrations for other apps."
-python manage.py migrate sessions
-
-echo "Apply migrations for other apps."
-python manage.py migrate sites
-
-echo "Apply migrations for other apps."
-python manage.py migrate socialaccount
-
-echo "Apply migrations for api."
-python manage.py migrate api 0002_vacancies_certificate_filter_follower_quizz_and_more
+echo "Apply migrations."
+python manage.py migrate
 
 echo "Create a superuser."
 python manage.py createsuperuser-ifnot
