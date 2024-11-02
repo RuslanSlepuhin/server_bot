dc21807ee79e61ade02de28c0bef61392a27f6b2
Mixail132 <millionbaxov@gmail.com>
2024-11-02 19:02:43 +0300

    Add a path to the database files.

--- _apps/itcoty_web/itcoty_web/dirs.py
+++ _apps/itcoty_web/itcoty_web/dirs.py
@@ -1,4 +1,8 @@
+import platform
 from pathlib import Path
+from itcoty_web.envs import load_config
+
+env = load_config()
 
 BASE_DIR = Path(__file__).resolve().parents[1]
 DOCKER_CACHE_DIR = Path("/var/cache/app")
@@ -8,6 +12,11 @@ STATIC_DIR = (
                  DOCKER_CACHE_DIR if DOCKER_CACHE_DIR.is_dir() else LOCAL_DIR
              ) / "static"
 
-
-LOCAL_DIR.mkdir(exist_ok=True, mode=0o777)
 STATIC_DIR.mkdir(exist_ok=True, mode=0o777)
+LOCAL_DIR.mkdir(exist_ok=True, mode=0o777)
+
+if platform.system() == "Windows":
+    BASE_DB_DIR = Path(f"{env.database.winpath}").resolve()
+    DB_DIR = BASE_DB_DIR / f"{env.database.version}"
+    DB_DATA = DB_DIR / "data"
+    DB_CTL = DB_DIR / "bin" / "pg_ctl"
